722019a98da2fe49bbc331e8ada5d0699792aaf9
rocio <rocio@rocioalcolea>
2022-08-28 11:21:47 +0200

    comenzando

---
+++ marvel/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+
+function App() {
+  return (
+    <div>
+      <p>Creando nuestra aplicacion de Marvel</p>
+      <div class="ui animated button" tabindex="0">
+        <div class="visible content">Next</div>
+        <div class="hidden content">
+          <i class="right arrow icon"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
